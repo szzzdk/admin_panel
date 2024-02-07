43d331aab50b2d948bd870ea200d03e51cf93976
Dariga <dariga.syzdykova05@gmail.com>
2024-02-07 19:14:01 +0600

    commmmit

--- src/components/AnalyticsLine.tsx
+++ src/components/AnalyticsLine.tsx
@@ -1,41 +1,13 @@
-import React, { useEffect, useRef } from 'react';
 
-interface AnalyticsData {
-  title: string;
-  sum: string;
-  arrow: string;
-  interval: string;
-  graph: string;
-}
+import { LineChart, Line } from 'recharts';
+import {data} from '../data';
 
-interface Props {
-  data: { totalAnalytics: AnalyticsData[] };
-}
-
-const AnalyticsLine: React.FC<Props> = ({ data }) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (canvasRef.current && data.totalAnalytics.length > 0) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-
-      if (ctx) {
-        // Очищаем холст перед рисованием нового текста
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Устанавливаем стиль текста
-        ctx.font = '20px Arial';
-        ctx.fillStyle = 'purple';
-
-        // Отображаем значения из данных
-        const analytics = data.totalAnalytics[0];
-        ctx.fillText(analytics.title + ': ' + analytics.sum, 10, 30);
-      }
-    }
-  }, [data]);
-
-  return <canvas ref={canvasRef} width={200} height={50}></canvas>;
+const AnalyticsLine = () => {
+  return (
+    <LineChart width={400} height={400} data={data}>
+      <Line type="monotone" dataKey="tp" stroke="#8884d8" />
+    </LineChart>
+  );
 };
 
 export default AnalyticsLine;
--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,21 +1,21 @@
 import AnalyticsLine from "../components/AnalyticsLine";
-import {data} from '../data';
+
+const graph = [
+    {
+        "title": "Total Patients",
+        "sum": "44000",
+        "arrow": "",
+        "interval": "",
+        "graph": ""
+    }
+]
 
 const Home = () => {
     return (
         <div>
             <div className="grid grid-cols-4 gap-4 h-56 items-stretch w-40">
                 <div>
-                    <AnalyticsLine data={data}/>
-                </div>
-                <div>
-                    <AnalyticsLine data={data} />
-                </div>
-                <div>
-                    <AnalyticsLine data={data} />
-                </div>
-                <div>
-                    <AnalyticsLine  data={data}/>
+                    <AnalyticsLine />
                 </div>
             </div>
         </div>
